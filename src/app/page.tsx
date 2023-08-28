cd1b340b30e7db672c5759f351bd4f5469dd0929
BadgerV <segunfaozan@gmail.com>
2023-08-28 02:01:23 +0100

    final commit

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { prisma } from "@/db";
+import { TodoItem } from "@/components/TodoItem";
+
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+export async function toggleTodo(id:string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({where : {id}, data : {complete}})
+
+} 
+
+export default async function Home() {
+  const todos = await getTodos()
+  // await prisma.todo.create({data : {title : "test", complete : false}})
+  return (
+    <>
+      <header className="flex justify-between mb-4 items-center">
+        <h1 className="text-2xl">Todos</h1>
+        <Link href = "/new" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hoverbg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key = {todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </>
+  );
+}
